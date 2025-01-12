42774494103ad24f0fb1b25f1e304280efe6e649
vothanhnhan14 <36332457+vothanhnhan14@users.noreply.github.com>
2025-01-12 16:23:36 +1030

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,2 +1,8 @@
 from django.http import HttpResponse
-print("OK")
+from django.views.decorators.csrf import csrf_exempt
+from django.http.request import MultiPartParser
+
+class MyClass:
+    @csrf_exempt  # To avoid CSRF token issues for demonstration
+    def handle_multipart_form(self, request):
+        print("OK")
